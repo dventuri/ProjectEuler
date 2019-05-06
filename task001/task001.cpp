fdf574fa7c7f594cdd3a6cbcfe76153712b29585
dventuri <diegoventuri@gmail.com>
2019-05-06 19:00:22 -0300

    moved main calculation to function

--- task001/task001.cpp
+++ task001/task001.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 
-int main() {
-    unsigned int sum = 0;
+unsigned int sumMultiplesUpTo(int mult1, int mult2, int upTo);
 
-    for(int i = 1; i < 1000; i++)
-    {
-        if (!(i % 3) || !(i % 5) ) {
-            sum += i;
-        }
-    }
+int main() {
+    unsigned int sum;
+
+    sum = sumMultiplesUpTo(3, 5, 1000);
 
     std::cout << sum << std::endl;
 
     return 0;
+}
+
+unsigned int sumMultiplesUpTo(int mult1, int mult2, int upTo) {
+    unsigned int sum = 0;
+    for (int i = 1; i < upTo; i++) {
+        if (!(i % mult1) || !(i % mult2) ) {
+            sum += i;
+        }
+    }
+
+    return sum;
 }
