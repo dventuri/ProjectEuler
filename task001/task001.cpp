409bad934edf98365731c44718a541e213b4f0b8
dventuri <diegoventuri@gmail.com>
2019-05-06 19:17:11 -0300

    added alternate function

--- task001/task001.cpp
+++ task001/task001.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 unsigned int sumMultiplesUpTo(int mult1, int mult2, int upTo);
+unsigned int sumDivisibleBy(int n);
 
 int main() {
     unsigned int sum;
 
     sum = sumMultiplesUpTo(3, 5, 1000);
+    std::cout << sum << std::endl;
 
+    sum = sumDivisibleBy(3)+sumDivisibleBy(5)-sumDivisibleBy(3*5);
     std::cout << sum << std::endl;
 
     return 0;
@@ -21,4 +24,12 @@ unsigned int sumMultiplesUpTo(int mult1, int mult2, int upTo) {
     }
 
     return sum;
+}
+
+unsigned int sumDivisibleBy(int n) {
+    int target = 1000-1;
+    int p;
+
+    p = target / n;
+    return n*(p*(p+1)) / 2;
 }
